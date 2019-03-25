aa22a5c18b2e395af41781524c9b24aab8de0b70
Aury Bocker <aurybocker@gmail.com>
2019-03-24 22:35:53 -0600

    Add files via upload

---
+++ src/main/java/com/equifax/selenium/testwebdrive/TestwebdriveApplication.java
@@ -0,0 +1,13 @@
+package com.equifax.selenium.testwebdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestwebdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestwebdriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equifax/selenium/testwebdrive/TestwebdriveApplicationTests.java
@@ -0,0 +1,47 @@
+package com.equifax.selenium.testwebdrive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestwebdriveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.setProperty("webdriver.chrome.driver","C:\\tools\\trabajo\\proyectos\\testwebdrive\\webdriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		String baseUrl = "http://demo.guru99.com/test/newtours/";
+		String expectedTitle = "Welcome: Mercury Tours";
+		String actualTitle = "";
+
+		// launch Fire fox and direct it to the Base URL
+		driver.get(baseUrl);
+
+		// get the actual value of the title
+		actualTitle = driver.getTitle();
+
+		/*
+		 * compare the actual title of the page with the expected one and print
+		 * the result as "Passed" or "Failed"
+		 */
+		if (actualTitle.contentEquals(expectedTitle)){
+			System.out.println("Test Passed!");
+		} else {
+			System.out.println("Test Failed");
+		}
+
+		//close Fire fox
+		driver.close();
+
+	}
+
+
+}
+
+
